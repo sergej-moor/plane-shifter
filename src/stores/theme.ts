d290ca76f423295c4f589616db87b41680cbf0fa
Sergej Moor <sergejmoor01@gmail.com>
2024-11-29 15:52:22 +0100

    feat: theming for tweakpane ui

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,12 +1,29 @@
 import { writable } from "svelte/store";
+import { ThemeUtils } from "svelte-tweakpane-ui";
+
+export type Theme = "light" | "dark";
 
 // Get initial theme from URL
 const url = new URL(window.location.href);
-const initialTheme = url.searchParams.get("theme") || "";
+const initialTheme = (url.searchParams.get("theme") as Theme) || "";
 
-export const theme = writable(initialTheme);
+// Create the store
+export const theme = writable<Theme>(initialTheme);
+
+// Helper function to apply theme
+function applyThemeToTweakpaneUI(newTheme: Theme) {
+  if (newTheme === "dark") {
+    ThemeUtils.setGlobalDefaultTheme(ThemeUtils.presets.jetblack);
+  } else {
+    ThemeUtils.setGlobalDefaultTheme(ThemeUtils.presets.light);
+  }
+}
 
 // Helper function to update theme
-export function updateTheme(newTheme: string) {
+export function updateTheme(newTheme: Theme) {
+  applyThemeToTweakpaneUI(newTheme);
   theme.set(newTheme);
 }
+
+// Apply initial theme
+applyThemeToTweakpaneUI(initialTheme);
